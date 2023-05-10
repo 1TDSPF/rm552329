ff2ffcf28f40566fc5052afeae687e8256a849e9
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-05-09 21:34:47 -0300

    Realizando os js dos exercicios e adicionando as imagens

--- exercicio-javascript-1/exercicio-1.js
+++ exercicio-javascript-1/exercicio-1.js
@@ -1,10 +1,29 @@
-const btn = document.getElementById("btn1");
+const btn = document.getElementById("btn-cor");
 btn.addEventListener("click", function(){
-    document.body.style.backgroundColor = "red";
+    let r,g,b;
+    r = Math.round(Math.random() *255);
+    g = Math.round(Math.random() *255);
+    b = Math.round(Math.random() *255);
+
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
 });
 
-const button = document.getElementById("btn2");
+const botao = document.getElementById("btn-troca");
+    botao.addEventListener("click", function(){
+      const imgElemento = document.querySelector("#btn-img img");
+      imgElemento.src = "./esquilo2.jpg";
+      imgElemento.alt = "Nova Imagem";
+    });
+
+const button = document.getElementById("btn-exibir");
 button.addEventListener("click", function(){
     const mensagemElemento = document.getElementById("mensagem");
     mensagemElemento.innerHTML = "Mensagem exibida!";
 })
+
+const botao5 = document.getElementById("btn-oculta");
+    const imagemElemento = document.getElementById("imagem-oculta");
+
+    botao5.addEventListener("click", function(){
+      imagemElemento.style.display = "none";
+    });
