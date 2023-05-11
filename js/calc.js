5d2910880ba22e0644f10145d6b5198d2a9ea29a
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-05-11 16:07:24 -0300

    Realizando a estilização da calculadora com o css e parte do javascript, usando função e parametro

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+//Criando uma função para receber o botão
+
+function insereNumero(botao,visor){
+    visor.value += botao.value;
+
+}
+
+// function resultado(visor){
+//     let novoVisor = visor.value.split("+")
+//     let resultadoOperacao = 0;
+
+//     novoVisor.forEach((nr)=>{
+//         resultadoOperacao += parseInt(nr);
+//     })
+//     console.log(resultadoOperacao)    
+// }
+
+function resultado(visor){
+
+    let soma = visor.value.split("+")
+    console.log(soma);
+    let subtracao = visor.value.split("-")
+    console.log(subtracao);      
+}
