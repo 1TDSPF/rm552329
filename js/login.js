3ebde62485c019717656900a9c307fac6f1ccf43
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-05-18 15:07:48 -0300

    Realizando o local-storage no token, usuario validado

--- js/login.js
+++ js/login.js
@@ -1,47 +1,60 @@
-// //CRIAR UMA LISTA DE OBJETOS
-// let listaDeUsuarios = [
-//     {
-//         nomeCompleto : "Jaú da Selva",
-//         usuarioEmail : "jau@email.com",
-//         usuarioSenha : "123456"
-//     },
-//     {
-//         nomeCompleto : "Paulo das Couves",
-//         usuarioEmail : "paulo@email.com",
-//         usuarioSenha : "123456"
-//     },
-//     {
-//         nomeCompleto : "José da Silva",
-//         usuarioEmail : "jose@email.com",
-//         usuarioSenha : "123456"
-//     },
-//     {
-//         nomeCompleto : "Maria do Socorro",
-//         usuarioEmail : "maria@email.com",
-//         usuarioSenha : "123456"
-//     },
-//     {
-//         nomeCompleto : "André da Silva",
-//         usuarioEmail : "andre@email.com",
-//         usuarioSenha : "123456"
-//     }
-// ];
+//CRIAR UMA LISTA DE OBJETOS
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "Jaú da Selva",
+        usuarioEmail : "jau@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar: "./img/usuario-padrao.png"
+    },
+    {
+        nomeCompleto : "Paulo das Couves",
+        usuarioEmail : "paulo@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar: "./img/usuario-padrao.png"
+    },
+    {
+        nomeCompleto : "José da Silva",
+        usuarioEmail : "jose@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar: "./img/usuario-padrao.png"
+    },
+    {
+        nomeCompleto : "Maria do Socorro",
+        usuarioEmail : "maria@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar: "./img/usuario-padrao.png"
+    },
+    {
+        nomeCompleto : "André da Silva",
+        usuarioEmail : "andre@email.com",
+        usuarioSenha : "123456",
+        usuarioAvatar: "./img/usuario-padrao.png"
+    }
+];
 
 // //ADICIONANDO A LISTA DE OBJETOS AO LOCAL-STORAGE
-// localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
+//localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt)=>{
     
     const inputEmail = document.querySelector("#idEmail");
     const inputPass = document.querySelector("#idPass");
-    
+    //CRIANDO UM OBJETO PARA ARMAZENAR OS DADOS QUE VIERAM DO FORM
+    const userLogado = {
+        emailUserLogado : inputEmail.value,
+        senhaUserLogado : inputPass.value
+    }
+    //CRIANDO UM OBJETO PARA ARMAZENAR O USUARIO VALIDADO
+    let userValidado ={};
     if(evt.target.id == "btnSubmit"){
 
         try {
             
+            let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUser"));
             listaDeUsuarios.forEach((usuario)=>{
      
-                if(inputEmail.value == usuario.usuarioEmail && inputPass.value == usuario.usuarioSenha){
+                if(userLogado.emailUserLogado == usuario.usuarioEmail && userLogado.senhaUserLogado == usuario.usuarioSenha){
+                    userValidado = usuario;
                     throw "VALIDADO";
                 }
             });
@@ -53,11 +66,26 @@ addEventListener("click", (evt)=>{
             
             if(msg == "VALIDADO"){
                 msgStatus.setAttribute("style","color:#00ff00;");
-                msgStatus.innerHTML = "<span><strong>Login efetuado com Sucesso!</strong></span>";
+                msgStatus.innerHTML = `<span><strong>O usuário ${userValidado.nomeCompleto} realizou o login efetuado com Sucesso!</strong></span>`;
+               
+                //ADICIONANDO O USUARIO VALIDADO NO LOCAL-STORAGE
+                localStorage.setItem("user-validado", JSON.stringify(userValidado))
+                //CRIAR O TOKEN DE AUTENTICAÇÃO DO USUÁRIO
+                    let token = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+                    console.log(token); 
+
+                //ADICIONANDO O TOKEN NO LOCAL -STORAGE
+                    localStorage.setItem("token-user", token); 
+                    
+                 //REDIRECT
+                setTimeout(()=>{
+                    window.location.href = "../index.html";
+                }, 3000);
             }else{
                 msgStatus.setAttribute("style","color:#ff0000;");
                 msgStatus.innerHTML = "<span><strong>Senha ou nome de usuário inválidos!</strong></span>";
-            }
+           
+                }
             
         }
     //Ao mostrar a senha troque o ícone também pelo ícone de olho com um traço.
